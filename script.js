df938f154f053428c1249e3e31c887a0fef8b7d3
PavelSeroglazlin <mr.seroglazkin@bk.ru>
2022-05-25 18:54:52 +0400

    Изменен файл script.js, добавлен файл hamburger.html

--- script.js
+++ script.js
@@ -5,27 +5,50 @@ const goods = [
   { title: 'Shoes', price: 250, images: 'images/24.jpg' },
 ];
 
-const renderGoodsItem = ({ title = '', price = 0, images = null }) =>
-    `
-<div class="grid-item">
-  <div class="grid-img">
-    <img src="${images}" width="300" height="300" alt="chandler">
-  </div>
-  <hr>
-    <div class="product_catalog">
-      <div class="catalog-item">${title}</div>
-      <ul class="catalog-item">
-        <li class="catalog-item_price"><b>${price}</b> руб.</li>
-        <li><a class="add-to-cart-link" href="cart/add?id=3"><img src="images/pannier.png"
-          alt="pannier"></a></li>
-      </ul>
-    </div>
-</div>
-`;
+class GoodsItem {
+  constructor({ title, price, images }) {
+    this.title = title;
+    this.price = price;
+    this.images = images;
+  }
+  render() {
+    return `
+    <div class="grid-item">
+		<div class="grid-img">
+			<img src="${this.images}" width="300" height="300" alt="chandler">
+		</div>
+		<hr>
+		<div class="product_catalog">
+			<div class="catalog-item">${this.title}</div>
+			<ul class="catalog-item">
+				<li class="catalog-item_price"><b>${this.price}</b> руб.</li>
+				<li><a class="add-to-cart-link" href="cart/add?id=3"><img src="images/pannier.png"
+				alt="pannier"></a></li>
+			</ul>
+		</div>
+	</div>
+  `;
+  }
+}
+class GoodsList {
+  items = [];
+  fetchGoods() {
+    this.items = goods;
+  }
+  render() {
+    const goods = this.items.map(item => {
+      const goodItem = new GoodsItem(item);
+      return goodItem.render()
+    }).join('');
 
-const renderGoodsList = (list = []) => {
-  let goodsList = list.map(item => renderGoodsItem(item)).join('');
-  document.querySelector('.goods-list').innerHTML = goodsList;
+    document.querySelector('.goods-list').innerHTML = goods;
+  }
+  getCount() {
+    return Object.values(goods).reduce((acc, product) => acc + product.price, 0);
+  }
 }
 
-renderGoodsList(goods);
+const goodsList = new GoodsList();
+goodsList.fetchGoods();
+goodsList.render();
+console.log(goodsList.getCount());
